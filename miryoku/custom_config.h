cffcef9030fb3237bf6fcc5e09ce4be1c1c905ae
Hanif Norman <hanifnorman@gmail.com>
2023-04-06 13:03:10 +0800

    Move gaming layer under ifdef

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -14,39 +14,14 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
-// add new gaming layer
-#define MIRYOKU_LAYER_GAME \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
-&kp Z,             U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
-
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(GAME,   "Gaming") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun")
-
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_GAME   2
-#define U_TAP    3
-#define U_BUTTON 4
-#define U_NAV    5
-#define U_MOUSE  6
-#define U_MEDIA  7
-#define U_NUM    8
-#define U_SYM    9
-#define U_FUN    10
-
 #if defined (MIRYOKU_KEYBOARD_CORNEISH_ZEN)
+	// define EXTRA layer as dedicated gaming layer
+	#define MIRYOKU_LAYER_EXTRA \
+	&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+	&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
+	&kp Z,             U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+	U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
 	// define extra keys on corne-ish zen for all layers
 	#define MIRYOKU_LAYOUTMAPPING_CORNE( \
 		K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -60,7 +35,7 @@ MIRYOKU_X(FUN,    "Fun")
 						K32  K33  K34       K35  K36  K37
 
 	// assign extra keys for gaming layer
-	#define MIRYOKU_LAYOUTMAPPING_GAME( \
+	#define MIRYOKU_LAYOUTMAPPING_EXTRA( \
 		K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
 		K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
 		K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
